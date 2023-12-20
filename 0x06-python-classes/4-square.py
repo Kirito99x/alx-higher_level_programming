f449452ef72ee0cba6278884a3a113a1e5865363
Kirito99x <msaifeldin46@gmail.com>
2023-12-20 23:14:43 +0200

    103-magic_class.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -20,19 +20,19 @@ class Square:
 
         self.size = size
 
-        @property
-        def size(self):
-            """get the size of square"""
-            return (self._size)
+    @property
+    def size(self):
+        """get the size of square"""
+        return (self._size)
 
-        @size.setter
-        def size(self, value):
-            if not isinstance(size, int):
-                raise TypeError("size must be an integer")
-            elif size < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = size
+    @size.setter
+    def size(self, value):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
-        def area(self):
-            """return the are of the square """
-            return (self.__size * self.__size)
+    def area(self):
+        """return the are of the square """
+        return (self.__size * self.__size)
