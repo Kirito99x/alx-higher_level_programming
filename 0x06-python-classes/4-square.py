9b3a77ceb0d95c4be13c8f2e181940e488d15867
Kirito99x <msaifeldin46@gmail.com>
2023-12-20 23:11:41 +0200

    103-magic_class.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -12,6 +12,12 @@ class Square:
         Args:
             size: The size of square.
         """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
         self.size = size
 
         @property
