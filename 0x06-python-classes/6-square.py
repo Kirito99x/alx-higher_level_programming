afd07abacbbc47d1aabf84c015de497557ab970f
Kirito99x <msaifeldin46@gmail.com>
2023-12-20 23:20:49 +0200

    103-magic_class.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,76 +1,60 @@
 #!/usr/bin/python3
-"""A module defining the Square class.
 
-Raises:
-    TypeError: If size is not an integer.
-    ValueError: If size is less than 0.
-"""
+"""Define a class Square."""
 
 
 class Square:
-    """Class representing a square.
+    """Represent a square."""
 
-    Attributes:
-        __size (int): The size of the square.
-
-    Methods:
-        area(): Calculate the area of the square.
-    """
-
-    def __init__(self, size=0):
-        """Initialize square with a given size.
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize a new square.
 
         Args:
-            size (int): The size of the square. Defaults to 0.
-
-        Raises:
-            TypeError: If size is not an integer.
-            ValueError: If size is less than 0.
+            size (int): The size of the new square.
+            position (int, int): The position of the new square.
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-
-        if size < 0:
-            raise ValueError("size must be >= 0")
-
-        self.__size = size
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
-        """Get the size of the square."""
-        return self.__size
+        """Get/set the current size of the square."""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        """Set the size of the square.
-
-        Args:
-            value (int): The size of the square.
-
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is less than 0.
-        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
-
         self.__size = value
 
-    def area(self):
-        """Calculate the area of the square.
+    @property
+    def position(self):
+        """Get/set the current position of the square."""
+        return (self.__position)
 
-        Returns:
-            int: The area of the square.
-        """
-        return self.__size * self.__size
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
 
     def my_print(self):
-        """Print the square."""
+        """Print the square with the # character."""
         if self.__size == 0:
-            print()
-        else:
-            for i in range(self.__size):
-                print("#" * self.__size)
+            print("")
+            return
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
