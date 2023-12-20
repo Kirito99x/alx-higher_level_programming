4023de677e399d6563107ce15939bad548942f3c
Kirito99x <msaifeldin46@gmail.com>
2023-12-20 22:57:01 +0200

    103-magic_class.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -14,10 +14,12 @@ class Square:
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
+        
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
         
+        self.__size = size
+
     def area(self):
         """return the are of the square """
         return self.__size * self.__size
