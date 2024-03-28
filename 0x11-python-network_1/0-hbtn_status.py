f60e7f950c58cb09b0d560e01de7f6fa02c6bd38
Kirito99x <msaifeldin46@gmail.com>
2024-03-28 23:15:30 +0200

    py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """Python script that fetches https://alx-intranet.hbtn.io/status"""
-import  urllib.request
+if __name__ == '__main__':
+    import  urllib.request
 
-url = "http://alx-intranet.hbtn.io/status"
+    url = "http://alx-intranet.hbtn.io/status"
 
-with urllib.request.urlopen(url) as response:
-    body = response.read().decode('utf-8')
+    with urllib.request.urlopen(url) as response:
+        body = response.read().decode('utf-8')
 
-print("/t- Body response:")
-print("/t/t- type:", type(body))
-print("/t/t- content:", (body))
+    print("/t- Body response:")
+    print("/t/t- type:", type(body))
+    print("/t/t- content:", (body))
