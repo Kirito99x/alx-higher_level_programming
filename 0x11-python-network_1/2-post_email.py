c9d3f6d18c48a8d953d40fbccb83cd469d56ef87
Kirito99x <msaifeldin46@gmail.com>
2024-03-29 00:09:48 +0200

    py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""A python script that:
+- takes in a URL
+- sends a POST request to the passed URL
+- takes email as a param
+- displays the body response
+"""
+import sys
+import urllib.parse
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = {"your email is": sys.argv[2]}
+    body = urllib.parse.urlencode(email).encode("ascii")
+
+    request = urllib.request.Request(url, body)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
