7c70d138bc1e663a73e819edc964e052ab10d384
Kirito99x <msaifeldin46@gmail.com>
2024-03-29 00:49:18 +0200

    py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""fetch url https://intranet.hbtn.io/status"""
+"""fetch url https://alx-intranet.hbtn.io/status"""
 import requests
 
 
 if __name__ == "__main__":
-    re = requests.get("https://intranet.hbtn.io/status")
+    re = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(re.text)))
     print("\t- content: {}".format(re.text))
