cc808b5cc2f435854954ded5d297f494c42d6e5b
Kirito99x <msaifeldin46@gmail.com>
2024-03-28 23:21:30 +0200

    py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     with urllib.request.urlopen(url) as response:
         body = response.read().decode('utf-8')
 
-    print("\t- Body response:")
-    print("\t- type:", type(body))
-    print("\t- content:", (body))
-    print("\t- utf8 content: {}".format(body.decode('utf-8')))
+        print("\t- Body response:")
+        print("\t- type:", type(body))
+        print("\t- content:", (body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
