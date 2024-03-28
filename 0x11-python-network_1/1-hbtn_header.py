b4d8d2553cd7b66a6a410307ba95b9229094b884
Kirito99x <msaifeldin46@gmail.com>
2024-03-28 23:53:42 +0200

    py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""a Python script that takes in a URL, 
+"""a Python script that 
+-takes in a URL, 
 -sends a request to the URL  
 -displays the value of the X-Request-Id 
 """
-
 import urllib.request
 import sys
 
