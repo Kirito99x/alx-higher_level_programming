9b513535be84f1a038e665b3a634275c87840a6c
Kirito99x <msaifeldin46@gmail.com>
2024-03-28 23:35:12 +0200

    py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""A python script that fetches https://alx-intranet.hbtn.io/status and uses urlib package
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
 """
+
 
 
 if __name__ == '__main__':
